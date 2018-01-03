40acdc30c0a22eb31b14d4451254ea387c85ffbb
Anujkmr <anujkmr974@gmail.com>
2018-01-03 17:23:50 +0530

    issues solved

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,6 +1,8 @@
 app.controller("weatherCntr", function ($scope, weatherFactory, NgMap) {
+    $scope.plus = 0;
     var cityObject = {};
     $scope.cityData = [];
+    $scope.array = [];
     $scope.submitForm = function () {
         var cityName = $scope.cityName;
         //time of usa
@@ -18,9 +20,107 @@ app.controller("weatherCntr", function ($scope, weatherFactory, NgMap) {
                 , temp: data.data.list[0].main
             }
             $scope.cityData.push(cityObject);
-            var array = $scope.cityData[$scope.cityData.length - 1];
-            console.log("array", array);
-            $scope.myValues = [array.temp.humidity, array.temp.temp_max, array.temp.temp_min, array.temp.pressure, array.temp.sea_level];
+            //object to store on temprature data
+            //            tempratureObj = {
+            //                humidity: cityObject.temp.humidity
+            //                , max_temp: cityObject.temp.temp_max
+            //                , min_temp: cityObject.temp.temp_min
+            //                , pressure: cityObject.temp.pressure
+            //                , sea_level: cityObject.temp.sea_level
+            //            }
+            tempratureObj = [cityObject.temp.humidity, cityObject.temp.temp_max, cityObject.temp.temp_min, cityObject.temp.pressure, cityObject.temp.sea_level];
+            $scope.array.push(tempratureObj);
+            console.log("array", $scope.array);
+            //            var array = $scope.cityData[$scope.cityData.length - 1];
+            //            var key = Object.keys(array.temp);
+            //            var len = key.length;
+            //object
+            //                        $scope.myValues = [array.temp.humidity, array.temp.temp_max, array.temp.temp_min, array.temp.pressure, array.temp.sea_level];
+            //            var zingchart = angular.element("<div zingchart zc-values='myValues' zc-json='myJson' zc-width='100%' zc-height='100%'></div>");
+            //            var target = document.getElementById('resizable');
+            //    angular.element(resizable).append(zingchart);
+            //                        for (i = 0; i < array.length; i++) {
+            //                            $scope.nfLines[vm.nflLines[i].AwayAbbrev] = {
+            //                                [Zingchart json configuration goes here]
+            //                            }
+            //                        }
+            function thejson() {
+                $scope.myJson = {
+                    title: {
+                        text: "Weather Report"
+                        , fontSize: 16
+                        , fontColor: "#fff"
+                    }
+                    , backgroundColor: "#2bbb9a"
+                    , globals: {
+                        shadow: false
+                        , fontFamily: "Arial"
+                    }
+                    , type: "line"
+                    , scaleX: {
+                        markers: []
+                        , labels: ['Humidity', 'Temp_max', 'Temp_min', 'Pressure', 'Sea_level']
+                        , maxItems: 8
+                        , lineColor: "white"
+                        , lineWidth: "1px"
+                        , tick: {
+                            lineColor: "white"
+                            , lineWidth: "1px"
+                        }
+                        , item: {
+                            fontColor: "white"
+                        }
+                        , guide: {
+                            visible: false
+                        }
+                    }
+                    , scaleY: {
+                        lineColor: "white"
+                        , lineWidth: "1px"
+                        , tick: {
+                            lineColor: "white"
+                            , lineWidth: "1px"
+                        }
+                        , guide: {
+                            lineStyle: "solid"
+                            , lineColor: "#249178"
+                        }
+                        , item: {
+                            fontColor: "white"
+                        }
+                    , }
+                    , tooltip: {
+                        visible: false
+                    }
+                    , crosshairX: {
+                        lineColor: "#fff"
+                        , scaleLabel: {
+                            backgroundColor: "#fff"
+                            , fontColor: "#323232"
+                        }
+                        , plotLabel: {
+                            backgroundColor: "#fff"
+                            , fontColor: "#323232"
+                            , text: "%v"
+                            , borderColor: "transparent"
+                        }
+                    }
+                    , plot: {
+                        lineWidth: "2px"
+                        , lineColor: "#FFF"
+                        , aspect: "spline"
+                        , marker: {
+                            visible: false
+                        }
+                    }
+                    , series: [{
+                        //                        values: [$scope.array[$scope.plus].humidity, $scope.array[$scope.plus].max_temp, $scope.array[$scope.plus].min_temp, $scope.array[$scope.plus].pressure, $scope.array[$scope.plus].sea_level]
+    }]
+                }
+                $scope.plus++;
+                console.log("plus", $scope.plus);
+            }
+            thejson();
             $scope.myObj = {
                 series: [
                     {
@@ -38,7 +138,6 @@ app.controller("weatherCntr", function ($scope, weatherFactory, NgMap) {
             $scope.result = error;
         });
         //chart
-        console.log("ddddd", $scope.hu);
         //
         //chart end
         //map
